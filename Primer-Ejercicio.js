93a8824f21147b1a6232c585f850efc80fa00d27
Maicol Socha <maisochajimenez@gmail.com>
2025-05-19 17:25:10 -0500

    Primer ejercicio en los retos falta por acabar

---
+++ Primer-Ejercicio.js
@@ -0,0 +1,40 @@
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+
+/* https://www.oracle.com/developer/javascript/ */
+
+// El doble slash funciona para crear un comentario de una linea
+
+/*  El slash asterisco
+    funciona para crear un comentario para mas de 1 linea */
+
+let variable = "Esta es una variable let puede cambiar su valor"
+const constante = "Esta es una constante const no puede cambiar su valor"
+
+/* En javascript hay 7 tipos de datos primitivos:
+    
+
+*/
