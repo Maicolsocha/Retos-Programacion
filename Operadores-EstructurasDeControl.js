dfbd35faa4b1ef064894a4e7c6f59f1ed57483f1
Maicol Socha <maisochajimenez@gmail.com>
2025-06-11 17:16:24 -0500

    Operadores y estructuras (2 ejercicio) un falta por terminar

---
+++ Operadores-EstructurasDeControl.js
@@ -0,0 +1,24 @@
+// Operadores Aritméticos
+let suma = `Esto es suma ${5 + 5}`;
+let resta = `Esto es resta ${6 - 9}`;
+let multiplicacion = `Esto es multiplicacion ${7 * 8}`;
+let division = `Esto es division ${25 / 0}`;
+let modulo = `Esto es modulo ${10 % 2}`;
+let potencia = `Esto es potencia ${2 ** 3}`;
+let incremento = `Esto es incremento ${suma++}` // Se puede colocar antes o despues de la variable
+let decremento = `Esto es decremento ${resta--}` // Se puede colocar antes o despues de la variable
+
+
+// Operadores Logicos
+
+let and = `Operador AND ${true && false}`; // Logico y las dos expresiones deben ser verdaderas
+let or = `Operador or ${true || true}` // Logico o al menos una de las expresiones debe ser verdadera
+let NOT = `Operador NOT ${!true}` // Logico NOT invierte el valor de la expresion
+
+// Operadores De Comparacion
+
+let mayorQue = `Esto es mayor que ${7 > 3}`
+let menorQue = `Esto es menor que ${7 < 0}`
+let mayorOIgualQue = `Esto es mayor o igual que ${7 >= 7}`
+let igualQue = `Esto es igual que ${7 == 7}`
+let igualestricto = `Esto es igual estricto que ${7 === 7}` // esta igualdad debe ser el mismo valor y el mismo tipo
